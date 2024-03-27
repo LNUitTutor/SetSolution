bae05fcb969b097b7004a2f877422e1d6de1d968
LNUitTutor <lnu.it.tutor@gmail.com>
2024-03-27 20:01:06 +0200

    Creation tests added

--- SetApplication/SetApplication.cpp
+++ SetApplication/SetApplication.cpp
@@ -4,15 +4,15 @@
 int main()
 {
     //Set<int> A;
-    //std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    //std::cout << " A = " << A << " has size " << A.size() << '\n';
     //int a[5] = { 1, 8, 1, 5, 0 };
     //A.add(5);
-    //std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    //std::cout << " A = " << A << " has size " << A.size() << '\n';
     //A.addRange(a, 5);
-    //std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    //std::cout << " A = " << A << " has size " << A.size() << '\n';
     //Set<int> B(A);
-    //std::cout << " B = " << B << " has size " << B.Size() << '\n';
-    //std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    //std::cout << " B = " << B << " has size " << B.size() << '\n';
+    //std::cout << " A = " << A << " has size " << A.size() << '\n';
     //for (int x : a)
     //{
     //    std::cout << x << " belongs " << A.contain(x) << '\n';
@@ -20,22 +20,22 @@ int main()
     //std::cout << 10 << " belongs " << A.contain(10) << '\n';
     int a[5] = { 1, 8, 1, 5, 0 };
     Set<int> A(a, 5);
-    std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
     int* d = A.to_array();
-    for (int i = 0; i < A.Size(); ++i) std::cout << ' ' << d[i];
+    for (int i = 0; i < A.size(); ++i) std::cout << ' ' << d[i];
     std::cout << '\n';
     delete[] d;
     int b[4] = { 2, 8, 3, 8 };
     Set<int> B(b, 4);
-    std::cout << " B = " << B << " has size " << B.Size() << '\n';
+    std::cout << " B = " << B << " has size " << B.size() << '\n';
     Set<int> C = A.set_union(B);
-    std::cout << " A+B = " << C << " has size " << C.Size() << '\n';
+    std::cout << " A+B = " << C << " has size " << C.size() << '\n';
     A.remove(1);
-    std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
     A.remove(0);
-    std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
     A.remove(8);
-    std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
     try
     {
         A.remove(10);
@@ -45,13 +45,19 @@ int main()
         std::cout << ex.what() << '\n';
     }
     for (int i = 3; i < 37; i += 5) A.add(i);
-    std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
     Set<int> P = A.intersect(B);
-    std::cout << " A*B = " << P << " has size " << P.Size() << '\n';
+    std::cout << " A*B = " << P << " has size " << P.size() << '\n';
     Set<int> R = A.intersect(C);
-    std::cout << " A*C = " << R << " has size " << R.Size() << '\n';
+    std::cout << " A*C = " << R << " has size " << R.size() << '\n';
     Set<int> Q = A.difference(B.add(33));
-    std::cout << " A-B-{33} = " << Q << " has size " << Q.Size() << '\n';
+    std::cout << " A-B-{33} = " << Q << " has size " << Q.size() << '\n';
     Q.clear();
-    std::cout << " Q = " << Q << " has size " << Q.Size() << '\n';
+    std::cout << " Q = " << Q << " has size " << Q.size() << '\n';
+    int c[] = { 10, 9, 8, 3, 4, 5, 7, 6, 1, 2, 1, 2, 3 };
+    Set<int> C1(c, sizeof c / sizeof * c);
+    std::cout << " C1 = " << C1 << " has size " << C1.size() << '\n';
+    int* e = C1.to_array();
+    for (int i = 1; i <= 10; ++i) std::cout << e[i-1];
+    delete[] e;
 }
--- SetUnitTests/SetUnitTests.cpp
+++ SetUnitTests/SetUnitTests.cpp
@@ -3,15 +3,84 @@
 #include "..\SetClass\Set.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
+/*
+	Set<T>& operator=(const Set<T>& other);
+	Set<T>& add(const T& x);
+	Set<T>& addRange(T* arr, int n)
+	Set<T>& remove(const T& x);
+	Set<T>& clear()
+	int Size() const { return this->size; }
+	bool contain(const T& x)
+	Set<T> set_union(const Set<T>& other);
+	Set<T> intersect(const Set<T>& other);
+	Set<T> difference(const Set<T>& other);
+	void printOn(std::ostream& os) const
+	T* to_array() const
+*/
+template <typename T>
+bool AreEqual(T* expected, T* actual, int size)
+{
+	for (int i = 0; i < size; ++i)
+		if (expected[i] != actual[i]) return false;
+	return true;
+}
 namespace SetUnitTests
 {
 	TEST_CLASS(SetCreationTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestDefaultCreation)
 		{
+			Set<int> A;
+			Set<double> B;
+			Set<char> C;
+			Assert::AreEqual(0, A.size());
+			Assert::AreEqual(0, B.size());
+			Assert::AreEqual(0, C.size());
+		}
+		TEST_METHOD(TestAnElementCreation)
+		{
+			Set<int> A(5);
+			Set<double> B(2.71);
+			Set<char> C('C');
+			Assert::AreEqual(1, A.size());
+			Assert::IsTrue(A.contain(5));
+			Assert::AreEqual(1, B.size());
+			Assert::IsTrue(B.contain(2.71));
+			Assert::AreEqual(1, C.size());
+			Assert::IsTrue(C.contain('C'));
+		}
+		TEST_METHOD(TestARangeCreation)
+		{
+			int a[] = { 1, 1, 1, 1, 1 };
+			int b[] = { 1, 2, 3, 4, 5 };
+			int c[] = { 10, 9, 8, 3, 4, 5, 7, 6, 1, 2, 1, 2, 3 };
+			Set<int> A(a, 5);
+			Set<int> B(b, 5);
+			Set<int> C(c, sizeof c / sizeof * c);
+			Assert::AreEqual(1, A.size());
+			Assert::IsTrue(A.contain(1));
+			Assert::AreEqual(5, B.size());
+			int* d = B.to_array();
+			//for (int i = 0; i < 5; ++i) Assert::AreEqual(b[i], d[i]);
+			Assert::IsTrue(AreEqual(b, d, 5));
+			delete[] d;
+			Assert::AreEqual(10, C.size());
+			int* e = C.to_array();
+			for (int i = 0; i < 10; ++i) Assert::AreEqual(i + 1, e[i]);
+			delete[] e;
+		}
+		TEST_METHOD(TestCopyCreation)
+		{
+			int b[] = { 1, 2, 3, 4, 5 };
+			Set<int> B(b, 5);
+			Set<int> C(B);
+			Assert::AreEqual(B.size(), C.size());
+			int* e = C.to_array();
+			//for (int i = 0; i < 5; ++i) Assert::AreEqual(b[i], e[i]);
+			Assert::IsTrue(AreEqual(b, e, 5));
+			delete[] e;
 		}
 	};
 }
