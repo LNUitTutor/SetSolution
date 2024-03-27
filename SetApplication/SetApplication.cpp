f92d79964779829654a55c2de60f77ccebe62201
LNUitTutor <lnu.it.tutor@gmail.com>
2024-03-27 18:00:03 +0200

    Separate library and application

---
+++ SetApplication/SetApplication.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "..\SetClass\Set.h"
+
+int main()
+{
+    //Set<int> A;
+    //std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    //int a[5] = { 1, 8, 1, 5, 0 };
+    //A.add(5);
+    //std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    //A.addRange(a, 5);
+    //std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    //Set<int> B(A);
+    //std::cout << " B = " << B << " has size " << B.Size() << '\n';
+    //std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    //for (int x : a)
+    //{
+    //    std::cout << x << " belongs " << A.contain(x) << '\n';
+    //}
+    //std::cout << 10 << " belongs " << A.contain(10) << '\n';
+    int a[5] = { 1, 8, 1, 5, 0 };
+    Set<int> A(a, 5);
+    std::cout << " A = " << A << " has size " << A.Size() << '\n';
+    int b[4] = { 2, 8, 3, 8 };
+    Set<int> B(b, 4);
+    std::cout << " B = " << B << " has size " << B.Size() << '\n';
+    Set<int> C = A.set_union(B);
+    std::cout << " C = " << C << " has size " << C.Size() << '\n';
+}
