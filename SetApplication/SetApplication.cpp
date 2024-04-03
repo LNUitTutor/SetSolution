810c73ca311c9eb4792cb0cd46d435903e8c02e5
LNUitTutor <lnu.it.tutor@gmail.com>
2024-04-03 17:19:24 +0300

    Alternate sieve added

--- SetApplication/SetApplication.cpp
+++ SetApplication/SetApplication.cpp
@@ -1,24 +1,22 @@
 ﻿#include <iostream>
 #include "..\SetClass\Set.h"
 
+void TestFirst();
+void TestSecond();
+void EratospheneSieve();
+void EratospheneSieve_1();
+
 int main()
 {
-    //Set<int> A;
-    //std::cout << " A = " << A << " has size " << A.size() << '\n';
-    //int a[5] = { 1, 8, 1, 5, 0 };
-    //A.add(5);
-    //std::cout << " A = " << A << " has size " << A.size() << '\n';
-    //A.addRange(a, 5);
-    //std::cout << " A = " << A << " has size " << A.size() << '\n';
-    //Set<int> B(A);
-    //std::cout << " B = " << B << " has size " << B.size() << '\n';
-    //std::cout << " A = " << A << " has size " << A.size() << '\n';
-    //for (int x : a)
-    //{
-    //    std::cout << x << " belongs " << A.contain(x) << '\n';
-    //}
-    //std::cout << 10 << " belongs " << A.contain(10) << '\n';
-    /*int a[5] = {1, 8, 1, 5, 0};
+   /* TestFirst();
+    TestSecond();*/
+    EratospheneSieve();
+    EratospheneSieve_1();
+
+}
+void TestFirst()
+{
+    int a[5] = {1, 8, 1, 5, 0};
     Set<int> A(a, 5);
     std::cout << " A = " << A << " has size " << A.size() << '\n';
     int* d = A.to_array();
@@ -67,26 +65,83 @@ int main()
         std::cout << *start << ' ';
         ++start;
     }
-    std::cout << '\n';*/
+    std::cout << '\n';
+}
+void TestSecond()
+{
+    Set<int> A;
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
+    int a[5] = { 1, 8, 1, 5, 0 };
+    A.add(5);
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
+    A.addRange(a, 5);
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
+    Set<int> B(A);
+    std::cout << " B = " << B << " has size " << B.size() << '\n';
+    std::cout << " A = " << A << " has size " << A.size() << '\n';
+    for (int x : a)
+    {
+        std::cout << x << " belongs " << A.contain(x) << '\n';
+    }
+    std::cout << 10 << " belongs " << A.contain(10) << '\n';
+}
 
+void EratospheneSieve()
+{
     std::cout << "Input the upper integer: ";
     unsigned n; std::cin >> n;
+    // build a total set
     Set<unsigned> prime;
     for (unsigned i = 2; i <= n; ++i) prime.add(i);
+    // get the iterators to enumerate through the set
     Set<unsigned>::Iterator start = prime.begin();
     Set<unsigned>::Iterator stop = prime.end();
     while (start != stop)
     {
         unsigned member = *start;
+        // every next multilie is bigger then previous one by the adder
         unsigned adder = member;
         member += adder;
         while (member <= n)
         {
+            // remove can causes an exception
             try { prime.remove(member); }
             catch (...) {}
+            // next multiplie
             member += adder;
         }
-        ++start;
+        ++start; // next prime number in the set
     }
     std::cout << "prime = " << prime << '\n';
+    std::cout << "size = " << prime.size() << '\n';
+}
+void EratospheneSieve_1()
+{
+    std::cout << "Input the upper integer: ";
+    unsigned n; std::cin >> n;
+    // build a total set
+    Set<unsigned> prime;
+    for (unsigned i = 2; i <= n; ++i) prime.add(i);
+    // start with the first prime number
+    unsigned member = 2;
+    while (member < n)
+    {
+        // every multiple of the member has form "member*k" where k=2,3,...
+        unsigned k = 2;
+        unsigned next_number = member * k;
+        // remove present multiplies
+        while (next_number <= n)
+        {
+            if (prime.contain(next_number)) prime.remove(next_number);
+            ++k;
+            next_number = member * k;
+        }
+        // look for the next prime memeber in the set
+        do
+        {
+            ++member;
+        } while (!prime.contain(member) && member < n);
+    }
+    std::cout << "prime = " << prime << '\n';
+    std::cout << "size = " << prime.size() << '\n';
 }
